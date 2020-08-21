c1a8eca14cdab93d2b8fb58fedecf0f3ab564b23
Danila Stryzhonok <36641744+ark23CIS@users.noreply.github.com>
2020-08-22 00:19:09 +0300

    adding a bit of styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,35 +6,40 @@ export default function Header() {
   const dispatch = useDispatch();
   const onClickCategory = React.useCallback(
     (e) => {
-      dispatch(fetchNews(e.currentTarget.innerHTML));
+      dispatch(fetchNews(e.currentTarget.innerHTML.toLowerCase()));
     },
     [dispatch]
   );
   return (
     <>
-      <ul className="ul-menu">
-        <li onClick={onClickCategory} className="ul-menu__item business">
-          business
-        </li>
-        <li onClick={onClickCategory} className="ul-menu__item enertainment">
-          entertainment
-        </li>
-        <li onClick={onClickCategory} className="ul-menu__item general">
-          general
-        </li>
-        <li onClick={onClickCategory} className="ul-menu__item health">
-          health
-        </li>
-        <li onClick={onClickCategory} className="ul-menu__item science">
-          science
-        </li>
-        <li onClick={onClickCategory} className="ul-menu__item sports">
-          sports
-        </li>
-        <li onClick={onClickCategory} className="ul-menu__item technology">
-          technology
-        </li>
-      </ul>
+      <header className="header">
+        <ul className="ul-menu">
+          <li onClick={onClickCategory} className="ul-menu__item business">
+            business
+          </li>
+          <li onClick={onClickCategory} className="ul-menu__item enertainment">
+            entertainment
+          </li>
+          <li onClick={onClickCategory} className="ul-menu__item general">
+            general
+          </li>
+          <li onClick={onClickCategory} className="ul-menu__item health">
+            health
+          </li>
+          <li onClick={onClickCategory} className="ul-menu__item science">
+            science
+          </li>
+          <li onClick={onClickCategory} className="ul-menu__item sports">
+            sports
+          </li>
+          <li onClick={onClickCategory} className="ul-menu__item technology">
+            technology
+          </li>
+        </ul>
+        <div>
+          <input type="text" />
+        </div>
+      </header>
     </>
   );
 }
